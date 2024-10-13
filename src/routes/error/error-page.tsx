a2c5c17452318ae5dd4340fb8c9e9c4325955bd4
Rathish <rathishvbr@gmail.com>
2024-10-13 21:57:39 +0200

    Updated error page

--- src/routes/error/error-page.tsx
+++ src/routes/error/error-page.tsx
@@ -2,12 +2,11 @@ import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+    <div className="flex flex-col items-center justify-center h-screen" id="error-page">
+      <h1 className="text-4xl font-bold">Oops!</h1>
+      <p className="text-lg">Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
