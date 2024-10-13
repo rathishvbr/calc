71d2f3fe4437e15798c3fe4759f071a4d597b004
Rathish <rathishvbr@gmail.com>
2024-10-13 22:05:36 +0200

    Refactor theme toggle component

--- src/components/theme/mode-toggle.tsx
+++ src/components/theme/mode-toggle.tsx
@@ -1,12 +1,12 @@
-import { Moon, Sun } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Laptop, Moon, Sun } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
 import { useTheme } from "@/components/theme/theme-provider"
 
 export function ModeToggle() {
@@ -18,18 +18,18 @@ export function ModeToggle() {
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="ml-2">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          <Sun className="h-[1.2rem] w-[1.2rem] mr-2" /> Light
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          <Moon className="h-[1.2rem] w-[1.2rem] mr-2" /> Dark
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+          <Laptop className="h-[1.2rem] w-[1.2rem] mr-2" /> System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
