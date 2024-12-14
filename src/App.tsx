79fb47b109d99150e2ef7c7a5abb4c8646e3dd8d
Rathish <rathishvbr@gmail.com>
2024-12-14 22:29:59 +0100

    Added rupee loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import GroovyWalkAnimation from '@/components/lotties/groovy-walk-animation';
+import RupeeFullLoader from '@/components/lotties/rupee-full-loader';
 import { NavBar } from '@/components/nav-bar/nav-bar'
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className='flex justify-center items-center h-[90vh]'>
-        <GroovyWalkAnimation />
-      </div>
+      <main className='h-[calc(100vh-64px)] flex justify-center items-center w-full'>
+        <div className='w-64 h-64'>
+          <RupeeFullLoader />
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/components/lotties/rupee-full-loader.tsx
@@ -0,0 +1,14 @@
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import rupee from './rupee.lottie';
+
+const RupeeFullLoader = () => {
+  return (
+    <DotLottieReact
+      src={rupee}
+      loop
+      autoplay
+    />
+  );
+};
+export default RupeeFullLoader;
+
